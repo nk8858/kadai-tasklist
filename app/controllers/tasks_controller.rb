258424f82be5893d12e129c90bee42e8bae456a1
naoto.kimura <kimuranaoto0000@icloud.com>
2020-10-10 06:53:09 +0000

    layoutsファイル編集

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -15,10 +15,10 @@ class TasksController < ApplicationController
     @task = Task.new(task_params)
     
     if @task.save
-      flash[:success] = 'Task が正常に投稿されました'
+      flash[:success] = 'タスクが正常に投稿されました'
       redirect_to @task
     else
-      flash.now[:denger] = 'Task　が投稿されませんでした'
+      flash.now[:denger] = 'タスクが投稿されませんでした'
       render :new
     end
   end
@@ -31,10 +31,10 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
     
     if @task.update(task_params)
-      flash[:success] = 'Task は正常に更新されました'
+      flash[:success] = 'タスクは正常に更新されました'
       redirect_to @task
     else
-      flash.now[:danger] = 'Task は更新されませんでした'
+      flash.now[:danger] = 'タスクは更新されませんでした'
       render :edit
     end
   end
@@ -43,14 +43,15 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
     @task.destroy
     
-    flash[:success] = 'Task は正常に削除されました'
+    flash[:success] = 'タスクは正常に削除されました'
     redirect_to tasks_url
   end
-end
 
-private
 
-# Strong Parameter
-def task_params
-  params.require(:task).permit(:content)
+  private
+  
+  # Strong Parameter
+  def task_params
+    params.require(:task).permit(:content)
+  end
 end
