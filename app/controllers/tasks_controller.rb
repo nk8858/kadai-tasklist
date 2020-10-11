87c4729f8f48db09f76e651a9479cc3881ae08ac
naoto.kimura <kimuranaoto0000@icloud.com>
2020-10-11 09:15:14 +0000

    DRY,validation,add column

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,11 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
   def index
     @tasks = Task.all
   end
   
   def show
-    @task = Task.find(params[:id])
+    
   end
   
   def new
@@ -24,11 +25,11 @@ class TasksController < ApplicationController
   end
   
   def edit
-    @task = Task.find(params[:id])
+   
   end
   
   def update
-    @task = Task.find(params[:id])
+   
     
     if @task.update(task_params)
       flash[:success] = 'タスクは正常に更新されました'
@@ -50,8 +51,12 @@ class TasksController < ApplicationController
 
   private
   
+  def set_task
+    @task = Task.find(params[:id])
+  end
+  
   # Strong Parameter
   def task_params
-    params.require(:task).permit(:content)
+    params.require(:task).permit(:content, :status)
   end
 end
