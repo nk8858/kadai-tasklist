090c6a7adc0ee6f58a30f263442fc3bdc3e92a4d
naoto.kimura <kimuranaoto0000@icloud.com>
2020-10-13 04:23:28 +0000

    add bootstrap design

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -19,7 +19,7 @@ class TasksController < ApplicationController
       flash[:success] = 'タスクが正常に投稿されました'
       redirect_to @task
     else
-      flash.now[:denger] = 'タスクが投稿されませんでした'
+      flash.now[:danger] = 'タスクが投稿されませんでした'
       render :new
     end
   end
