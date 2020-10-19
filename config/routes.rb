c3b7df6d1d7b0fbf5a4a66a2f3ee206e6d1347ee
naoto.kimura <kimuranaoto0000@icloud.com>
2020-10-19 10:47:38 +0000

    signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  root to: 'tasks#index'
   
-  resources :tasks
+  root to: 'tasks#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+  get 'login', to: 'sessions#new'
+  get 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
+  get 'signup', to: 'users#new'
+  resources :tasks 
+  resources :users , only: [:index, :show, :new, :create]
+ end
