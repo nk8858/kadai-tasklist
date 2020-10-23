e212c38c52efe8e95d9cdd67e84c65788057b4a2
naoto.kimura <kimuranaoto0000@icloud.com>
2020-10-23 13:32:33 +0000

    update vr1.02

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -54,6 +54,11 @@ class TasksController < ApplicationController
   
 
   # Strong Parameter
+  def task_params
+    params.require(:task).permit(:content, :status)
+  end
+  
+
   def correct_user
     @task = current_user.tasks.find_by(id: params[:id])
     unless @task
